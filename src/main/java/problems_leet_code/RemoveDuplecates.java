2fb0f97f6200d84bae765368e41dc0e42340797e
sergvlet <70774572+sergvlet@users.noreply.github.com>
2023-04-06 15:56:34 +0300

    Create README.md

--- src/main/java/problems_leet_code/RemoveDuplecates.java
+++ src/main/java/problems_leet_code/RemoveDuplecates.java
@@ -1,16 +1,16 @@
 package problems_leet_code;
 
-public class removeDuplecates {
+public class RemoveDuplecates {
     public static void main(String[] args) {
         int arr[] = {0, 0, 0, 1, 2, 2, 3, 3, 4, 5, 6, 6, 45, 45, 50, 66, 66, 77};
-        int temp = 0;
+        int j = 0;
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] != arr[i + 1]) {
-                arr[temp] = arr[i];
-                temp++;
+                arr[j] = arr[i];
+                j++;
             }
         }
-        arr[temp++] = arr[arr.length - 1];
+        arr[j++] = arr[arr.length - 1];
 
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
