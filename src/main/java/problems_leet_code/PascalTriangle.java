49f0be8fc641d0807504936b31bc3d635ffd4d00
sergvlet <70774572+sergvlet@users.noreply.github.com>
2023-04-20 17:30:34 +0300

    Create README.md

--- src/main/java/problems_leet_code/PascalTriangle.java
+++ src/main/java/problems_leet_code/PascalTriangle.java
@@ -1,7 +1,34 @@
 package problems_leet_code;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PascalTriangle {
     public static void main(String[] args) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        int numRows = 7;
 
+        triangle.add(new ArrayList<>());
+        triangle.get(0).add(1);
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prevRow = triangle.get(i - 1);
+
+            row.add(1);
+
+            for (int j = 1; j < i; j++) {
+                row.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+
+            row.add(1);
+
+            triangle.add(row);
+        }
+
+        System.out.println(triangle);
     }
+
+
 }
+
